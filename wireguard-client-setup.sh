49d8069b0e4e540218cb7c48416db8589ec42c59
Alex Master <23503669+smolbytes@users.noreply.github.com>
2021-04-24 21:48:40 -0400

    default port values and cosmetic changes

--- wireguard-client-setup.sh
+++ wireguard-client-setup.sh
@@ -21,8 +21,8 @@ fi
 echo "[+] Please enter the following to continue...                [+]"
 read -t 60 -p 'Server peer public key: ' wgserverpubkey
 read -t 60 -p 'Server peer IP/domain: ' wgserverIP
-read -t 60 -p 'Server peer UDP port: (\"y\" for 51820)' wgserverUDPport
-if wgserverUDPport == "y"
+read -t 60 -p 'Server peer UDP port: ("y" for 51820)' wgserverUDPport
+if [ wgserverUDPport == "y" ]; then
     wgserverUDPport = 51820
 fi
 read -s -t 60 -p 'Your client private key: ' wgclientprivkey
